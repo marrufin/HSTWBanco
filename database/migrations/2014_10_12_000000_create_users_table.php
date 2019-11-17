019a5599ea3cfd4fc56c1c29b8ebe16c63bc740c
marrufin <marrufojosecarlos@gmail.com>
2019-11-16 23:11:58 -0600

    [ADD] Se agregaron migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(191);
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email')->unique()->nullable(false);
+            $table->string('password')->nullable(false);
+            $table->tinyInteger('is_active')->default('1')->nullable(false);
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
