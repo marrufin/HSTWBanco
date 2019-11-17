6ae5e7ca173f43bc14b0677792e9a11d01e5c17d
marrufin <marrufojosecarlos@gmail.com>
2019-11-16 22:51:11 -0600

    Init commit

---
+++ database/migrations/2019_11_17_044721_create_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre',45)->nullable(false);
+            $table->string('descripcion',255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
